d68b4e2dad718ad65b108d335b2cea1197bd1fcb
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-05-31 01:42:30 +0600

    Firebase issue fixed and adding env

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -5,18 +5,18 @@ import { getStorage } from "firebase/storage";
 
 // web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAiz_YmOLFdD1B2iXNkn7hvhT5UxLPIjLU",
-  authDomain: "computersworldltd.firebaseapp.com",
-  projectId: "computersworldltd",
-  storageBucket: "computersworldltd.appspot.com",
-  messagingSenderId: "187041138443",
-  appId: "1:187041138443:web:39ba4f6cad9f88077e040e",
-  // apiKey: import.meta.env.VITE_APP_APIKEY,
-  // authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
-  // projectId: import.meta.env.VITE_APP_PROJECTID,
-  // storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
-  // messagingSenderId: import.meta.env.VITE_APP_MESSAGINGSENDERID,
-  // appId: import.meta.env.VITE_APP_APPID,
+  // apiKey: "AIzaSyAiz_YmOLFdD1B2iXNkn7hvhT5UxLPIjLU",
+  // authDomain: "computersworldltd.firebaseapp.com",
+  // projectId: "computersworldltd",
+  // storageBucket: "computersworldltd.appspot.com",
+  // messagingSenderId: "187041138443",
+  // appId: "1:187041138443:web:39ba4f6cad9f88077e040e",
+  apiKey: import.meta.env.VITE_APP_APIKEY,
+  authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECTID,
+  storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APP_APPID,
 };
 
 // Initialize Firebase
